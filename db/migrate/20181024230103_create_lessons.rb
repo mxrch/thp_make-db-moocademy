142d9a826b0a6683be57ceb69d740f6fc7e54867
Mxrshy <kvwaljam@protonmail.ch>
2018-10-25 01:07:40 +0200

    BDD créées

---
+++ db/migrate/20181024230103_create_lessons.rb
@@ -0,0 +1,11 @@
+class CreateLessons < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lessons do |t|
+      t.string :title
+      t.text :body
+      t.references :course, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
